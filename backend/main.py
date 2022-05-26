0a36467adb4fce57846e164b2678eae989a930b8
0gravity000 <0gravit000@gmail.com>
2022-05-26 18:15:44 +0900

    フロントエンドとバックエンドのやりとりできるよう設定を変更

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import logging
 
-app = Flask(__name__)
+# instantiate the app
+# Frontend側で生成した画面をBackend側で表示するようにする
+app = Flask(__name__, static_folder='./dist/static', template_folder='./dist') 
+#app = Flask(__name__)
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+# LEVEL を DEBUG に変更
+logging.basicConfig(level=logging.DEBUG)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=('GET', 'POST'))
+def index(path):
+    return render_template('index.html')
 
 if __name__ == '__main__':
   app.run()
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:'../backend/dist', // npm run build 時のファイルの出力先ルート
+  assetsDir: 'static', //staticフォルダ名を指定(js、css、imgフォルダをひとくくりにする）
+  devServer: {
+    port: 8080,
+    // localhostでvueからflaskにAPIリクエストを送信する為の設定
+    proxy: 'http://localhost:5000'
+  },
+  pages: {  // Titleの表示をFrontendから変更する
+    index: {
+      entry: "src/main.js",
+      title: "ForTesting",
+    }
+  },
 })
